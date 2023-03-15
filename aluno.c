71905b4e1f2c23b3213950b36a8720bbe153ddfc
cauehcraft <caueholiveira@gotmail.com>
2023-03-15 16:11:09 -0300

    Aprimoramento do codigo

--- aluno.c
+++ aluno.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 #include "aluno.h"
+#define tam_linha 100
 
 struct aluno {
     char nome[81];
@@ -41,6 +42,55 @@ void exibe_alunos (Aluno **alunos, int n_alunos) {
         printf("Matricula: %d\n", alunos[i]->matricula);
         printf("Documento: %d\n", alunos[i]->documento);
     }
+}
+
+int obter_alunos(Aluno* alunos){
+    int nlinhas =0;
+    FILE *al_file, *resultados;
+    char * linha = (char*) malloc(tam_linha*sizeof(char));
+    //int nlinhas = 0;
+    //char nome[100];
+    //int matricula, documento;
+
+    //verificando alocacao de linha
+    if (linha == NULL) printf("Erro de alocação.");
+    //lendo arquivos
+    al_file = fopen("dados_alunos.txt", "r+");
+    //verificando se o arquivo foi lido com sucesso.
+    if (al_file == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        exit(1);
+    }
+   //lendo cada linha do arquivo, salvando os dados
+    /*
+    while((fgets(linha, sizeof(linha), al_file))!=NULL){
+        printf("%s", linha); 
+        sscanf(linha, "%20[\t];%d;%d", &alunos[nlinhas].nome, &alunos[nlinhas].matricula, &alunos[nlinhas].documento);
+        (nlinhas)++;
+        //fprintf(resultados, "%s;%d;%d\n", nome, matricula, documento);
+    }*/
+    while (fscanf(al_file, "%[^;];%d;%d\n", alunos[nlinhas].nome, &alunos[nlinhas].matricula, &alunos[nlinhas].documento) != EOF) {
+        printf("Nome: %s\n", alunos[nlinhas].nome);
+        printf("Matricula: %d\n", alunos[nlinhas].matricula);
+        printf("Documento: %d\n\n", alunos[nlinhas].documento);
+        (nlinhas)++;
+    }
+
+
+    int i;
+    printf("----------ALUNOS----------\n");
+    for (i = 0; i < nlinhas; i++) {
+        printf("\nAluno %d:\n", i+1);
+        printf("Nome: %s\n", alunos[i].nome);
+        printf("Matricula: %d\n", alunos[i].matricula);
+        printf("Documento: %d\n", alunos[i].documento);
+    }
+    printf("\nTotal de alunos: %d\n", nlinhas);
+
+
+    if(fclose(al_file)!=0) printf("Erro ao fechar arquivo.");
+    free(linha);
+    return nlinhas;
 }
 
 /* Funcao que libera os espaco de memoria ocupado por um tipo aluno */
--- main.c
+++ main.c
@@ -2,49 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 #include "aluno.c"
-#define tam_linha 100
-
-void obter_dados(int* nlinhas, char* nome, int* matricula, int* documento){
-    FILE *al_file, *resultados;
-    char * linha = (char*) malloc(tam_linha*sizeof(char));
-    //int nlinhas = 0;
-    //char nome[100];
-    //int matricula, documento;
-
-    //verificando alocacao de linha
-    if (linha == NULL) printf("Erro de alocação.");
-    //lendo arquivos
-    al_file = fopen("dados_linhas.txt", "rt");
-    //resultados = fopen("resultados.txt", "wt");
-    //verificando se o arquivo foi lido com sucesso.
-    if (al_file == NULL) {
-        printf("Erro ao abrir o arquivo.\n");
-        exit(1);
-    }
-   //lendo cada linha do arquivo, salvando os dados
-    while((fgets(linha, tam_linha, al_file))!=NULL){
-        printf("%s", linha); 
-        sscanf(linha, "%[^;]s;%d;%d", &nome[*nlinhas], &matricula[*nlinhas], &documento[*nlinhas]);
-        (*nlinhas)++;
-        //fprintf(resultados, "%s;%d;%d\n", nome, matricula, documento);
-    }
-    printf("\nNumero de linhas: %d\n", *nlinhas);
 
 
-    if(fclose(al_file)!=0) printf("Erro ao fechar arquivo.");
-    free(linha);
-}
+
 
 int main (void) {
     //int matricula = 0, nlinhas = 0, documento = 0;
-    int *nlinhas = 0;
     char * nomes = (char*) malloc(100*sizeof(char));
     int * matriculas = (int*) malloc(100*sizeof(int));
     int * documentos = (int*) malloc(100*sizeof(int));
+    Aluno *alunos = (Aluno*) malloc(200*sizeof(Aluno));
+    
+    
+    int menu = 1, op;
 
-    obter_dados(nlinhas, nomes, matriculas, documentos);
+    while(menu == 1){
+        int total_alunos = obter_alunos(alunos);
+        printf("- Menu -\n1-9: opcoes...\n0- Sair.\n");
+        scanf("%d", &op);
+        switch(op){
+        case 0:
+            menu = 0;
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        default:
+            break;
+        }
+    }
 
-
+    /*
     Aluno *aluno1 = cria_aluno("Dayvison Silva", 12334, 111);
     Aluno *aluno2 = cria_aluno("Yeniffer Gustov", 23445, 222);
 
@@ -53,6 +44,10 @@ int main (void) {
     alunos[1] = aluno2;
 
     exibe_alunos(alunos, 2);
-    
+    */
+    free(nomes);
+    free(matriculas);
+    free(documentos);
+    free(alunos);
     return 0;
 }
