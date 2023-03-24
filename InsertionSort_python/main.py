1d2ff8c057e032899a05b4cbed29cf475bf4b7ec
August0n3 <august0n3211@gmail.com>
2023-03-24 12:39:18 -0300

    Finalizado o codigo em python

--- InsertionSort_python/main.py
+++ InsertionSort_python/main.py
@@ -1,8 +1,21 @@
 from aluno import Aluno
 from alunodao import AlunoDao
 
-#teste por enquanto
-aluno = Aluno('antrcos',111,123124)
-alunodao = AlunoDao()
-alunodao.cadastrar_aluno(aluno)
-alunodao.listar_aluno()
+op = 0
+
+while op != 3:
+    op = int(input('Menu\n1 - Cadastrar alunos\n2 - Listas alunos\n3 - sair\nDigite sua opção: '))
+
+    if op == 1:
+        nome = input('Digite o nome do aluno: ')
+        matricula = int(input('Digite a matricula do aluno: '))
+        documento = int(input('Digite o documento do aluno: '))
+        aluno = Aluno(nome,matricula,documento)
+        alunodao = AlunoDao()
+        alunodao.cadastrar_aluno(aluno)
+        alunodao.ordenar_alunos()
+        
+    elif op == 2:
+        alunodao = AlunoDao()
+        alunodao.listar_aluno()
+
